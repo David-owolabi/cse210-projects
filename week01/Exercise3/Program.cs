8badcd4f33114b38475b04930a1897fc49e020d3
David-owolabi <davidowolabi252@gmail.com>
2026-01-10 10:20:40 +0100

    Created a number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? ");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);  
+        int parseNumber = 0;
+
+
+        do
+        {
+        Console.WriteLine("What is your guess?");
+        string userGuess = Console.ReadLine();
+        parseNumber = int.Parse(userGuess); 
+
+        if (parseNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (parseNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        } 
+        while (parseNumber != magicNumber);
+
+        
+        
+
+        
     }
 }
