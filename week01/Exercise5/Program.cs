7e1ef7b03e4b717ad992a10491a4a368bbdb3ead
David-owolabi <davidowolabi252@gmail.com>
2026-01-14 17:14:53 +0100

    Practicing functions in C#

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    {   
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(name, squaredNumber); 
+
     }
+     static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+    
+        static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+            static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"Brother {name}, the square of your number is {squaredNumber}.");
+        }
+
 }
